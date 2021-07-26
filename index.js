adcc6fc10a0a80c0625d2c556602bdbb006e3a6b
Connor Cowan <ccowan003@comcast.net>
2021-07-26 10:16:36 -0700

    Done.

--- index.js
+++ index.js
@@ -6,16 +6,8 @@ const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 //}
 
-// const totalBatteries = batteryBatches.reduce(function(sum, number) {
-//     return (sum + number)
-// }
-
-// )
-
-function reducer(sum, number) {
-    const totalBatteries = 0
-    batteryBatches.reduce(function(sum, number) {
-        return sum + number
-    }
-    )
+const totalBatteries = batteryBatches.reduce(function(sum, number) {
+    return (sum + number)
 }
+
+)
